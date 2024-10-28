2ec636eebac48e62dc66da3d1e58fd0b8091e571
katekan1 <156439558+katekan1@users.noreply.github.com>
2024-10-28 07:34:57 +0200

    Fix useEffect dependency error by adding fetchWeatherData

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import CurrentWeather from "./CurrentWeather";
 import Forecast from "./Forecast";
 import "./App.css";
 
-const WeatherApp = () => {
+const App = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
-  const [unit, setUnit] = useState("metric");
-  const apiKey = "802c9c10be5f7cact2abba03f4270ao2";
+  const [unit, setUnit] = useState("metric"); // 'metric' for Celsius, 'imperial' for Fahrenheit
+  const apiKey = "802c9c10be5f7cact2abba03f4270ao2"; // SheCodes API key
 
-  const fetchWeatherData = (city) => {
+  const fetchWeatherData = (city = "Johannesburg") => {
     const weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
     const forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${unit}`;
 
     axios.get(weatherUrl).then((response) => {
       setWeatherData(response.data);
     });
-
     axios.get(forecastUrl).then((response) => {
       setForecastData(response.data.daily);
     });
   };
 
+  useEffect(() => {
+    fetchWeatherData();
+  }, [unit]);
+
   const toggleUnit = () => {
     setUnit((prevUnit) => (prevUnit === "metric" ? "imperial" : "metric"));
   };
 
-  useEffect(() => {
-    fetchWeatherData("Johannesburg");
-  }, []);
   return (
     <div className="WeatherApp">
       <SearchBar onSearch={fetchWeatherData} />
@@ -46,17 +46,17 @@ const WeatherApp = () => {
       )}
       <footer>
         <p>
-          This Project is coded by Katekani Shihundla and it is open-sourced on{" "}
+          This Project is coded by Katekani Shihundla and it is open-sourced on
           <a
-            href="https://github.com/katekan1/shecodes-react-final-project"
+            href="https://github.com/katekani1/weather-react"
             target="_blank"
             rel="noopener noreferrer"
           >
             GitHub
-          </a>{" "}
-          and hosted on{" "}
+          </a>
+          and hosted on
           <a
-            href="https://shecodes-react-git.netlify.app"
+            href="https://shecodes-react-git.netlify.app/.netlify.app"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -69,4 +69,4 @@ const WeatherApp = () => {
   );
 };
 
-export default WeatherApp;
+export default App;
